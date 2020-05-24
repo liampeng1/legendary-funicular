81f6d19b56b9febacb3d74e9a1557c87876cde6a
liampeng1 <pengliam3@gmail.com>
2020-05-23 20:33:08 -0500

    Co-authored-by: franklin.vannes@gmail.com <franklin.vannes@gmail.com>

--- JS/functions.js
+++ JS/functions.js
@@ -1,9 +1,41 @@
-function add_active_page() {
-    for (var page in document.getElementsByClassName(".nav_bar")){
-        if (page.href == window.location.href) {
-            page.addClass('active_page'); 
-        }
-        console.log();
-    }
-    console.log("done with add_active_page");
-}
+function updatePage(event) {
+    console.log(event);
+    const button = event.target;
+    const pageName = button.dataset.pageName;
+    updateContent(pageName);
+    updateNavBar(button);
+};
+
+function updateNavBar(button) {
+    console.log("updating nav_bar");
+    const curPage = document.querySelector(".curPage");
+    curPage.classList.remove("curPage");
+    button.classList.add("curPage");
+};
+
+function updateContent(pageName) {
+    const contentDiv = document.querySelector("#content");
+    fetch(pageName)
+        .then(response => {
+            console.log(response);
+            return response.text();
+        })
+        .then(text => {
+                const content = text;
+                console.log(content);
+                contentDiv.innerHTML = content;
+            }
+        );
+};
+
+function onPageLoaded() {
+    console.log("called onPageLoaded");
+    document.querySelectorAll('.nav_button').forEach(btn => {
+        btn.addEventListener('click', updatePage);
+    });
+    document.addEventListener('DOMContentLoaded', onPageLoaded);
+    updateContent("home.html");
+};
+
+onPageLoaded();
+console.log("helllo");
